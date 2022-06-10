177fb1f86296cb0f1212a957fce01c2f4a56de49
Panos-1993 <panoslampros@icloud.com>
2022-06-10 11:25:32 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import './components/header.js';
-import Header from './components/header.js';
-import Footer from './components/footer';
+import Header from './components/Header.js';
+import Footer from './components/Footer';
 import Profitability from './components/Profitability';
-import Liquidity from './components/liquidity';
+import Liquidity from './components/Liquidity';
 import Activity from './components/Activity';
 import Risk from './components/Risk';
-import About from './components/about.js';
+import About from './components/About.js';
 
 
 function App() {
